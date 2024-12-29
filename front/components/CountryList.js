eb67cccd8a380ef6e787cb16c0c569b7d8549031
KarolKozikowski <134882954+KarolKozikowski@users.noreply.github.com>
2024-12-29 21:56:08 +0100

    Country list done

--- front/components/CountryList.js
+++ front/components/CountryList.js
@@ -1,33 +1,107 @@
+import { GiAfrica } from "react-icons/gi";
+
 const CountryList = ({africa, asia, europe, northAmerica, southAmerica, oceania}) => (
-    <div>
-        <table>
-            <tbody>
-            <tr className="text-center"><th>Africa</th></tr>
-            {africa.map((item, index) => (
-                <tr key={index}><td>{item}</td></tr>
-            ))}
-            <tr><th>Asia</th></tr>
-            {asia.map((item, index) => (
-                <tr key={index}><td>{item}</td></tr>
-            ))}
-            <tr><th>Europe</th></tr>
-            {europe.map((item, index) => (
-                <tr key={index}><td>{item}</td></tr>
-            ))}
-            <tr><th>North America</th></tr>
-            {northAmerica.map((item, index) => (
-                <tr key={index}><td>{item}</td></tr>
-            ))}
-            <tr><th>South America</th></tr>
-            {southAmerica.map((item, index) => (
-                <tr key={index}><td>{item}</td></tr>
-            ))}
-            <tr><th>Oceania</th></tr>
-            {oceania.map((item, index) => (
-                <tr key={index}><td>{item}</td></tr>
-            ))}
-            </tbody>
-        </table>
+    <div className="grid grid-cols-3 gap-20 w-4/5 pb-20">
+        <div className="rounded-xl bg-white/[0.05]">
+            <table className="table table-striped w-full">
+                <thead>
+                <tr>
+                    <th className="flex flex-row py-5 pl-7 rounded-t-xl bg-white/[0.2]"><p
+                        className="basis-2/3 text-left text-xl font-normal">Africa</p><GiAfrica
+                        className="basis-1/3 h-8"/></th>
+                </tr>
+                </thead>
+                <tbody>
+                {africa.map((item, index) => (<tr key={index}>
+                    <td className="px-7 py-1.5 hover:bg-black/[0.2] cursor-pointer">{item}</td>
+                </tr>))}
+                </tbody>
+            </table>
+        </div>
+        <div className="rounded-xl bg-white/[0.05]">
+            <table className="table table-striped w-full">
+                <thead>
+                <tr>
+                    <th className="flex flex-row py-5 pl-7 rounded-t-xl bg-white/[0.2]"><p
+                        className="basis-2/3 text-left text-xl font-normal">Asia</p>
+                        <a className="basis-1/3 justify-items-center"><img src="/asia_outline.png" alt="continent symbol" className="h-8 opacity-90"/></a></th>
+                </tr>
+                </thead>
+                <tbody>
+                {asia.map((item, index) => (<tr key={index}>
+                    <td className="px-7 py-1.5 hover:bg-black/[0.2] cursor-pointer">{item}</td>
+                </tr>))}
+                </tbody>
+            </table>
+        </div>
+        <div className="rounded-xl bg-white/[0.05]">
+            <table className="table table-striped w-full">
+                <thead>
+                <tr>
+                    <th className="flex flex-row py-5 pl-7 rounded-t-xl bg-white/[0.2]"><p
+                        className="basis-2/3 text-left text-xl font-normal">Europe</p>
+                        <a className="basis-1/3 justify-items-center"><img src="/europe_outline.png" alt="continent symbol" className="h-8 opacity-90"/></a>
+                    </th>
+                </tr>
+                </thead>
+                <tbody>
+                {europe.map((item, index) => (<tr key={index}>
+                    <td className="px-7 py-1.5 hover:bg-black/[0.2] cursor-pointer">{item}</td>
+                </tr>))}
+                </tbody>
+            </table>
+        </div>
+        <div className="rounded-xl bg-white/[0.05]">
+            <table className="table table-striped w-full">
+                <thead>
+                <tr>
+                    <th className="flex flex-row py-5 pl-7 rounded-t-xl bg-white/[0.2]"><p
+                        className="basis-2/3 text-left text-xl font-normal">North America</p>
+                        <a className="basis-1/3 justify-items-center"><img src="/north_america_outline.png" alt="continent symbol" className="h-8 opacity-90"/></a>
+                    </th>
+                </tr>
+                </thead>
+                <tbody>
+                {northAmerica.map((item, index) => (<tr key={index}>
+                    <td className="px-7 py-1.5 hover:bg-black/[0.2] cursor-pointer">{item}</td>
+                </tr>))}
+                </tbody>
+            </table>
+        </div>
+        <div className="rounded-xl bg-white/[0.05]">
+            <table className="table table-striped w-full">
+                <thead>
+                <tr>
+                    <th className="flex flex-row py-5 pl-7 rounded-t-xl bg-white/[0.2]"><p
+                        className="basis-2/3 text-left text-xl font-normal">South America</p>
+                        <a className="basis-1/3 justify-items-center"><img src="/south_america_outline.png" alt="continent symbol" className="h-8 opacity-90"/></a>
+                    </th>
+                </tr>
+                </thead>
+                <tbody>
+                {southAmerica.map((item, index) => (<tr key={index}>
+                    <td className="px-7 py-1.5 hover:bg-black/[0.2] cursor-pointer">{item}</td>
+                </tr>))}
+                </tbody>
+            </table>
+        </div>
+        <div className="rounded-xl bg-white/[0.05]">
+            <table className="table table-striped w-full">
+                <thead>
+                <tr>
+                    <th className="flex flex-row py-5 pl-7 rounded-t-xl bg-white/[0.2]"><p
+                        className="basis-2/3 text-left text-xl font-normal">Oceania</p>
+                        <a className="basis-1/3 justify-items-center"><img src="/oceania_outline.png" alt="continent symbol" className="h-8 opacity-90"/></a>
+                    </th>
+                </tr>
+                </thead>
+                <tbody>
+                {oceania.map((item, index) => (<tr key={index}>
+                    <td className="px-7 py-1.5 hover:bg-black/[0.2] cursor-pointer">{item}</td>
+                </tr>))}
+                </tbody>
+            </table>
+        </div>
     </div>
 );
 export default CountryList;
--- front/pages/countries/index.js
+++ front/pages/countries/index.js
@@ -3,8 +3,11 @@ import CountryList from '@/components/CountryList';
 import React from "react";
 
 export default function CountriesPage(){
-    return(<div className="text-white justify-items-center">
-        <div className="text-center p-10 text-5xl font-thin">List of all countries</div>
+    return(<div className="text-white/[0.9] justify-items-center">
+        <div className="pt-24 pb-8 text-2xl font-thin w-4/5">
+            <h2>List of all countries</h2>
+            <hr className="h-px mt-2 bg-white/[0.5] border-0"/>
+        </div>
         <CountryList {...data.regions}/>
     </div>);
 }
