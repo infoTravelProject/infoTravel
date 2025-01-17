c8e2736f0088b329d281194f934c0c59d796df91
KarolKozikowski <134882954+KarolKozikowski@users.noreply.github.com>
2025-01-17 23:31:49 +0100

    settings - notifications: temporarily done

--- front/components/settings/Notifications.js
+++ front/components/settings/Notifications.js
@@ -1,5 +1,7 @@
 import {useGlobalContext} from "@/components/context/GlobalContext";
 import SettingsHeader from "@/components/settings/SettingsHeader";
+import Label from "@/components/Label";
+import ToggleBox from "@/components/settings/ToggleBox";
 
 const Notifications = () => {
     const {setSettingsPage} = useGlobalContext();
@@ -8,8 +10,15 @@ const Notifications = () => {
     return (
         <div>
             <SettingsHeader/>
-            <h1>Notifications</h1>
-            <p>This is the Notifications section.</p>
+            <Label text={"Email notifications"}/>
+            <ToggleBox text={"Login alerts"}/>
+            <ToggleBox text={"New features"}/>
+            <ToggleBox text={"Recommended events and points of interest"}/>
+            <ToggleBox text={"Special offers"}/>
+            <ToggleBox text={"Upcoming trips and events"} type={true}/>
+            <ToggleBox text={"Personalized news"} type={"disabled"}/>
+            <ToggleBox text={"Personalized travel recommendations"} type={"disabled"}/>
+            <ToggleBox text={"New travel guide releases"} type={"disabled"}/>
         </div>
     );
 };
--- front/components/settings/Preferences.js
+++ front/components/settings/Preferences.js
@@ -3,61 +3,14 @@ import SettingsHeader from "@/components/settings/SettingsHeader";
 import Button from "@/components/Button";
 import Label from "@/components/Label";
 import ToggleBox from "@/components/settings/ToggleBox";
+import tempData from "@/data/tempData.json";
+import tempLanguageData from "@/data/tempLanguageData.json";
 
 const Preferences = () => {
     const {setSettingsPage} = useGlobalContext();
     setSettingsPage("Preferences");
 
     //<temp>
-    const tempData = [
-        {
-            id: 1,
-            label: "I hate my eyes",
-            value: "ultra-light",
-        },
-        {
-            id: 2,
-            label: "Light",
-            value: "light",
-        },
-        {
-            id: 3,
-            label: "Dark",
-            value: "dark",
-        },
-        {
-            id: 4,
-            label: "Python developer",
-            value: "ultra-dark",
-        }
-    ];
-    const tempLanguageData =[
-        {
-            id: 1,
-            label: "English - US",
-            value: "enus",
-        },
-        {
-            id: 2,
-            label: "English - UK",
-            value: "enuk",
-        },
-        {
-            id: 3,
-            label: "Spanish",
-            value: "es",
-        },
-        {
-            id: 4,
-            label: "Polish",
-            value: "pl",
-        },
-        {
-            id: 5,
-            label: "German",
-            value: "de",
-        }
-    ];
     function mapper(data, value){
         let returnItem;
         data.map(item => {
